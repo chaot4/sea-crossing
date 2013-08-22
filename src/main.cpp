be914dbac6dbaf0c40c553e6144ccab21310089d
Chaot <git.andre.nusser@googlemail.com>
2013-08-22 14:40:13 +0200

    Adapted main.

--- src/main.cpp
+++ src/main.cpp
@@ -17,18 +17,15 @@ int main(){
 	//}
 	std::thread renderThread(&RenderHub::init,&testRenderer);
 	testReceiver->pushLoadSceneMessages();
-
-	testReceiver->pushCreateMarkerMessage("BSE", 0);
 	
-//	Board board;
-//	RandomAIPlayer player1("Spongebob", board);
-//	RandomAIPlayer player2("Patrick", board);
-//	
-//	ConsoleGame g(&player1, &player2, board);
-//	//g.start();
-//	std::thread gameThread(&ConsoleGame::start,&g);
-//
-//	gameThread.join();
+	Board board;
+	RandomAIPlayer player1("Spongebob", board);
+	RandomAIPlayer player2("Patrick", board);
+	
+	SimpleGUIGame<MessageReceiver> g(&player1, &player2, board, testReceiver);
+	std::thread gameThread(&SimpleGUIGame<MessageReceiver>::start,&g);
+
+	gameThread.join();
 	renderThread.join();
 
 }
