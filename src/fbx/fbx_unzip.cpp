00e99a23c8b2dfcbc7d73dc02b8cd38f41103623
Stefan Bühler <stbuehler@web.de>
2013-09-04 11:26:17 +0200

    embed miniz.c from https://miniz.googlecode.com/

--- src/fbx/fbx_unzip.cpp
+++ src/fbx/fbx_unzip.cpp
@@ -3,7 +3,47 @@
 
 extern "C" {
 	#include <string.h>
+#ifdef USE_ZLIB
 	#include <zlib.h>
+#else
+	// Define MINIZ_NO_STDIO to disable all usage and any functions which rely on stdio for file I/O.
+	#define MINIZ_NO_STDIO
+
+	// If MINIZ_NO_TIME is specified then the ZIP archive functions will not be able to get the current time, or
+	// get/set file times.
+	#define MINIZ_NO_TIME
+
+	// Define MINIZ_NO_ARCHIVE_APIS to disable all ZIP archive API's.
+	#define MINIZ_NO_ARCHIVE_APIS
+
+	// Define MINIZ_NO_ARCHIVE_APIS to disable all writing related ZIP archive API's.
+	#define MINIZ_NO_ARCHIVE_WRITING_APIS
+
+	// Define MINIZ_NO_ZLIB_APIS to remove all ZLIB-style compression/decompression API's.
+	//#define MINIZ_NO_ZLIB_APIS
+
+	// Define MINIZ_NO_ZLIB_COMPATIBLE_NAME to disable zlib names, to prevent conflicts against stock zlib.
+	#define MINIZ_NO_ZLIB_COMPATIBLE_NAMES
+
+	#include "miniz.c"
+
+	// #define's are dangerous. in this case, they'd rename FBX::inflate to FBX::mz_inflate.
+	// use proper aliasing instead.
+	typedef mz_stream z_stream;
+	static int inflateInit(z_stream* pStream) { return ::mz_inflateInit(pStream); }
+	static int inflateEnd(z_stream* pStream) { return ::mz_inflateEnd(pStream); }
+	static int inflate(z_stream* pStream, int flush) { return ::mz_inflate(pStream, flush); }
+
+	// these constants should be safe to #define - uppercase names are expected to be #define'd
+	#define Z_FINISH              MZ_FINISH
+	#define Z_OK                  MZ_OK
+	#define Z_STREAM_END          MZ_STREAM_END
+	#define Z_NEED_DICT           MZ_NEED_DICT
+	#define Z_STREAM_ERROR        MZ_STREAM_ERROR
+	#define Z_DATA_ERROR          MZ_DATA_ERROR
+	#define Z_MEM_ERROR           MZ_MEM_ERROR
+	#define Z_BUF_ERROR           MZ_BUF_ERROR
+#endif
 }
 
 namespace FBX {
