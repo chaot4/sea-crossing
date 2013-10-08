b4f536d35f717615de4943e17a905465e64a1fff
invor <michael.becher.1990@googlemail.com>
2013-10-08 15:32:22 +0200

    Initializer lists should now work with VS2013's compiler.

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -220,7 +220,7 @@ void DebugGame::command_forward()
 
 
 Game::Game(Player* player1, Player* player2, Board& board, GameConf const& conf)
-	: player{player1, player2}, board(board), conf(conf) {}
+	: player({player1, player2}), board(board), conf(conf) {}
 
 void Game::start()
 {
